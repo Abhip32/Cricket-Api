2a859a42ec76e2142fdf76200838f56eca0bd76d
Abhip32 <abhishek.a.patil112@gmail.com>
2024-02-08 01:00:21 +0530

    test run

--- controller/MatchDataController.js
+++ controller/MatchDataController.js
@@ -1,164 +1,51 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const flagController = require('./flagController');
-
-
 
 const MatchDataController = {
   getLiveMatches: async (req, res) => {
     try {
-      const response = await axios.get('https://www.cricketworld.com/cricket/live');
-      const html = response.data;
-      const $ = cheerio.load(html);
+      const [liveResponse, completedResponse, upcomingResponse] = await Promise.all([
+        axios.get('https://www.cricketworld.com/cricket/live'),
+        axios.get('https://www.cricketworld.com/cricket/completed'),
+        axios.get('https://www.cricketworld.com/cricket/upcoming')
+      ]);
 
-      const photos = [];
-      
-      const matches = $('.matches-table.fixtures.rt .match-row');
-      const matchDetails = [];
-      
-      matches.each((index, element) => {
-        const match = {};
-      
-        const teamA = $(element).find('.teama .teamName').text();
-        const flagA = $(element).find('.teama .teamLogo img').attr('src');
-        const flagB = $(element).find('.teamb .teamLogo img').attr('src');
-        const teamB = $(element).find('.teamb .teamName').text();
-        const scoreA= $(element).find('.teama .teamaScore').text();
-        const scoreB= $(element).find('.teamb .teamaScore').text();
-        const status = $(element).find('.status.status-1').text();
-        const day= $(element).find('.day').text();
-        const title= $(element).find('.title').text();
-        const subtitle = $(element).find('.subtitle').text();
-        const progress = $(element).find('.column-action').text();
-        const Link= $(element).attr('href');
-        
-      
-        match.teamA = teamA;
-        match.teamB = teamB;
-        match.flagA = flagA;
-        match.flagB = flagB;
-        match.scoreA= scoreA.replace(/\n/g, '');;
-        match.scoreB = scoreB.replace(/\n/g, '');;
-        match.status = status;
-        match.day= day;
-        match.title = title;
-        match.subtitle=subtitle;
-        match.progress= progress.replace(/\n/g, '');;
-        match.link=Link;
-      
-        matchDetails.push(match);
-      });    
+      const liveMatches = parseMatches(liveResponse.data);
+      const completedMatches = parseMatches(completedResponse.data);
+      const upcomingMatches = parseMatches(upcomingResponse.data);
 
-
-res.status(200).send(matchDetails);
-
+      res.status(200).send({ liveMatches, completedMatches, upcomingMatches });
     } catch (error) {
       console.log(error);
+      res.status(500).send('Internal Server Error');
     }
   },
-
-
-  getRecentMatches : async (req, res) => {
-    try {
-      const response = await axios.get('https://www.cricketworld.com/cricket/completed');
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      const photos = [];
-      
-      const matches = $('.matches-table.fixtures.rt .match-row');
-      const matchDetails = [];
-      
-      matches.each((index, element) => {
-        const match = {};
-      
-        const teamA = $(element).find('.teama .teamName').text();
-        const flagA = $(element).find('.teama .teamLogo img').attr('src');
-        const flagB = $(element).find('.teamb .teamLogo img').attr('src');
-        const teamB = $(element).find('.teamb .teamName').text();
-        const scoreA= $(element).find('.teama .teamaScore').text();
-        const scoreB= $(element).find('.teamb .teamaScore').text();
-        const status = $(element).find('.status.status-1').text();
-        const day= $(element).find('.day').text();
-        const title= $(element).find('.title').text();
-        const subtitle = $(element).find('.subtitle').text();
-        const progress = $(element).find('.column-action').text();
-        const Link= $(element).attr('href');
-      
-        match.teamA = teamA;
-        match.teamB = teamB;
-        match.flagA = flagA;
-        match.flagB = flagB;
-        match.scoreA= scoreA.replace(/\n/g, '');;
-        match.scoreB = scoreB.replace(/\n/g, '');;
-        match.status = status;
-        match.day= day;
-        match.title = title;
-        match.subtitle=subtitle;
-        match.progress= progress.replace(/\n/g, '');;
-        match.link=Link;
-      
-        matchDetails.push(match);
-      });
-
-        res.status(200).send(matchDetails);
-      
-    } catch (error) {
-      console.log(error);
-    }
-    
-  },
-
-  getUpcomingMatches:async (req,res)=>{
-    try {
-      const response = await axios.get('https://www.cricketworld.com/cricket/upcoming');
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      const photos = [];
-      
-      const matches = $('.matches-table.fixtures.rt .match-row');
-      const matchDetails = [];
-      
-      matches.each((index, element) => {
-        const match = {};
-      
-        const teamA = $(element).find('.teama .teamName').text();
-        const flagA = $(element).find('.teama .teamLogo img').attr('src');
-        const flagB = $(element).find('.teamb .teamLogo img').attr('src');
-        const teamB = $(element).find('.teamb .teamName').text();
-        const scoreA= $(element).find('.teama .teamaScore').text();
-        const scoreB= $(element).find('.teamb .teamaScore').text();
-        const status = $(element).find('.status.status-1').text();
-        const day= $(element).find('.day').text();
-        const title= $(element).find('.title').text();
-        const subtitle = $(element).find('.subtitle').text();
-        const progress = $(element).find('.column-action').text();
-        const Link= $(element).attr('href');
-      
-        match.teamA = teamA;
-        match.teamB = teamB;
-        match.flagA = flagA;
-        match.flagB = flagB;
-        match.scoreA= scoreA.replace(/\n/g, '');;
-        match.scoreB = scoreB.replace(/\n/g, '');;
-        match.status = status;
-        match.day= day;
-        match.title = title;
-        match.subtitle=subtitle;
-        match.progress= progress.replace(/\n/g, '');;
-        match.link=Link;
-      
-        matchDetails.push(match);
-      });
-      
-      res.status(200).send(matchDetails);
-    } catch (error) {
-      console.log(error);
-    }
-  },
-
-
 };
+
+function parseMatches(html) {
+  const $ = cheerio.load(html);
+  const matches = $('.matches-table.fixtures.rt .match-row');
+  const matchDetails = [];
+
+  matches.each((index, element) => {
+    const match = {
+      teamA: $(element).find('.teama .teamName').text(),
+      flagA: $(element).find('.teama .teamLogo img').attr('src'),
+      flagB: $(element).find('.teamb .teamLogo img').attr('src'),
+      teamB: $(element).find('.teamb .teamName').text(),
+      scoreA: $(element).find('.teama .teamaScore').text().replace(/\n/g, ''),
+      scoreB: $(element).find('.teamb .teamaScore').text().replace(/\n/g, ''),
+      status: $(element).find('.status.status-1').text(),
+      day: $(element).find('.day').text(),
+      title: $(element).find('.title').text(),
+      subtitle: $(element).find('.subtitle').text(),
+      progress: $(element).find('.column-action').text().replace(/\n/g, ''),
+      link: $(element).attr('href')
+    };
+    matchDetails.push(match);
+  });
+
+  return matchDetails;
+}
 
 module.exports = MatchDataController;
